6eefe6b55b1b2a50e421f7b14eab057a3d8c651a
ianblakenz <ian@faaa.com.au>
2025-10-14 01:59:21 +1100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'inflight-rest-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Install the service worker and cache the static assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // Not in cache - fetch from network
+        return fetch(event.request);
+      }
+    )
+  );
+});
