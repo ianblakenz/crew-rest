59fefaced7132e92234aff021069173c1e46566c
Ian <ian@faaa.com.au>
2025-10-15 15:17:06 +1100

    Update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inflight-rest-cache-v12'; // Version bumped to v9
+const CACHE_NAME = 'inflight-rest-cache-v13'; // Version bumped to v9
 const urlsToCache = [
   './',
   './index.html',
